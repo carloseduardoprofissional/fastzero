d29cb409dbc5fd9be815684df6df6c707f783295
carloseduardoprofissional <carlos.eduardo.github.prof@gmail.com>
2025-07-31 09:43:54 -0300

    Feat: solving the exercise, create a new endpoint to return a html file as response; created a test to the new endpoint; improved the assert to the test test_read_root_deve_retornar_ok_e_ola_mundo

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,4 +10,15 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
 
     response = client.get('/')
 
-    assert response.status_code == HTTPStatus.OK and response.json() == {'message': 'Olá Mundo!'}
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_read_html_deve_retornar_ok_e_html():
+    client = TestClient(app)
+
+    response = client.get('/text')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.headers['content-type'] == 'text/html; charset=utf-8'
+    assert '<h1>Olá, FastAPI!</h1>' in response.text
